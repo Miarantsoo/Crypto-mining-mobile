5adc54749aa70402daad8fb1fe7db23b4804289b
Tsinjoniaina18 <tsinjoniaina.randriamirado@gmail.com>
2025-02-04 14:46:44 +0300

    update

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,131 +1,118 @@
 import {
-  IonPage,
-  IonContent,
-  IonInput,
-  IonButton,
-  IonText
+    IonPage,
+    IonContent,
+    IonInput,
+    IonButton,
+    IonText
 } from '@ionic/react';
 import { FaArrowRight } from "react-icons/fa6";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
 import Bg from "../../assets/bg.jpg";
 import './Login.css';
 
-type FormFields = {
-  email: string;
-  mdp: string;
-};
-
 const Login = () => {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<FormFields>({
-    defaultValues: {
-      email: '',
-      mdp: ''
-    },
-    mode: 'onChange'
-  });
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
 
-  const onSubmit: SubmitHandler<FormFields> = (data) => {
-    console.log(data);
-  };
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
 
-  return (
-      <IonPage>
-        <IonContent fullscreen>
-          <div
-              className="login-background"
-              style={{ backgroundImage: `url(${Bg})` }}
-          >
-            <div className="login-container">
-              <div className="login-form-wrapper">
-              <span className="login-title">
-                Connectez vous avec Connectify
-              </span>
+        const newErrors: { email?: string; password?: string } = {};
 
-                <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                  <div className="input-wrapper">
-                    <Controller
-                        control={control}
-                        name="email"
-                        rules={{
-                          required: "L'adresse mail est requise",
-                          pattern: {
-                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                            message: "Veuillez insérer un e-mail valide"
-                          }
-                        }}
-                        render={({ field }) => (
-                            <>
-                              <IonInput
-                                  label="Email"
-                                  labelPlacement="floating"
-                                  fill="outline"
-                                  type="email"
-                                  placeholder="exemple@domain.com"
-                                  className={`${errors.email ? 'ion-invalid' : ''}`}
-                                  onIonChange={(e) => field.onChange(e.detail.value)}
-                                  value={field.value || ''}
-                              />
-                              {errors.email && (
-                                  <IonText color="danger">
-                                    <small>{errors.email.message}</small>
-                                  </IonText>
-                              )}
-                            </>
-                        )}
-                    />
-                  </div>
+        if (!email) {
+            newErrors.email = "L'adresse mail est requise";
+            setEmail('');
+        } else {
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+            if (!emailRegex.test(email)) {
+                newErrors.email = "Veuillez insérer un e-mail valide";
+                setEmail('');
+            }
+        }
 
-                  <div className="input-wrapper">
-                    <Controller
-                        control={control}
-                        name="mdp"
-                        rules={{
-                          required: "Le mot de passe est requis"
-                        }}
-                        render={({ field }) => (
-                            <>
-                              <IonInput
-                                  label="Mot de passe"
-                                  labelPlacement="floating"
-                                  fill="outline"
-                                  type="password"
-                                  placeholder="••••••••"
-                                  className={`${errors.mdp ? 'ion-invalid' : ''}`}
-                                  onIonChange={(e) => field.onChange(e.detail.value)}
-                                  value={field.value || ''}
-                              />
-                              {errors.mdp && (
-                                  <IonText color="danger">
-                                    <small>{errors.mdp.message}</small>
-                                  </IonText>
-                              )}
-                            </>
-                        )}
-                    />
-                  </div>
+        if (!password) {
+            newErrors.password = "Le mot de passe est requis";
+            setPassword('');
+        }
 
-                  <IonButton
-                      type="submit"
-                      expand="block"
-                      className="login-button"
-                      disabled={isSubmitting}
-                  >
-                    <div className="login-button-content">
-                      <span>Continuer</span>
-                      <FaArrowRight className="login-button-icon" />
+        setErrors(newErrors);
+
+        if (Object.keys(newErrors).length === 0) {
+            const data = {
+                email: email,
+                password: password
+            };
+            console.log("Submitted data:", data);
+        }
+    };
+
+    return (
+        <IonPage>
+            <IonContent fullscreen>
+                <div
+                    className="login-background"
+                    style={{ backgroundImage: `url(${Bg})` }}
+                >
+                    <div className="login-container">
+                        <div className="login-form-wrapper">
+                          <span className="login-title">
+                            Connectez-vous avec Connectify
+                          </span>
+                            <form className="login-form" onSubmit={handleSubmit}>
+                                <div className="input-wrapper">
+                                    <IonInput
+                                        label="Email"
+                                        labelPlacement="floating"
+                                        fill="outline"
+                                        type="email"
+                                        placeholder="exemple@domain.com"
+                                        value={email}
+                                        onIonInput={(e) => setEmail(e.detail.value!)}
+                                        className={errors.email ? 'ion-invalid' : ''}
+                                    />
+                                    {errors.email && (
+                                        <IonText color="danger">
+                                            <small>{errors.email}</small>
+                                        </IonText>
+                                    )}
+                                </div>
+
+                                <div className="input-wrapper">
+                                    <IonInput
+                                        label="Mot de passe"
+                                        labelPlacement="floating"
+                                        fill="outline"
+                                        type="password"
+                                        placeholder="••••••••"
+                                        value={password}
+                                        onIonInput={(e) => setPassword(e.detail.value!)}
+                                        className={errors.password ? 'ion-invalid' : ''}
+                                    />
+                                    {errors.password && (
+                                        <IonText color="danger">
+                                            <small>{errors.password}</small>
+                                        </IonText>
+                                    )}
+                                </div>
+
+                                <IonButton
+                                    type="submit"
+                                    expand="block"
+                                    className="login-button"
+                                >
+                                    <div className="login-button-content">
+                                        <span>Continuer</span>
+                                        <FaArrowRight className="login-button-icon" />
+                                    </div>
+                                </IonButton>
+                            </form>
+                        </div>
                     </div>
-                  </IonButton>
-                </form>
-              </div>
-            </div>
-          </div>
-        </IonContent>
-      </IonPage>
-  );
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Login;

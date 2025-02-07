cf6e12f01003f52b0081658a2afff26d9d9f1533
El <irina.elina.r@gmail.com>
2025-02-07 13:26:16 +0300

    Revert "navbar"
    This reverts commit a039a557dcc537817ed0e7f026d79c55c1d48045.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { IonApp, setupIonicReact } from '@ionic/react';
 import { Redirect, Route } from 'react-router-dom';
-import {IonApp, IonContent, IonRouterOutlet, setupIonicReact} from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 
@@ -40,8 +40,6 @@ import Login from "./pages/login/Login";
 import PorteFeuille from "./pages/portefeuille/PorteFeuille";
 import Historique from "./pages/historique/Historique";
 import Solde from "./pages/solde/Solde";
-import Navbar from "./components/navigation/Navbar";
-import React from "react";
 setupIonicReact();
 
 const App: React.FC = () => {
@@ -57,27 +55,26 @@ const App: React.FC = () => {
 	return (
 		<IonApp>
 			<IonReactRouter>
-					<IonRouterOutlet>
-						<Route exact path="/home">
-							<Home />
-						</Route>
-						<Route exact path="/">
-							<Redirect to="/home" />
-						</Route>
-						<Route exact path="/login">
-							<Login />
-						</Route>
-						<Route exact path="/portefeuille">
-							<PorteFeuille />
-						</Route>
-						<Route exact path="/historique">
-							<Historique />
-						</Route>
-						<Route exact path="/solde">
-							<Solde />
-						</Route>
-					</IonRouterOutlet>
-				<Navbar />
+				<IonRouterOutlet>
+					<Route exact path="/home">
+						<Home />
+					</Route>
+					<Route exact path="/">
+						<Redirect to="/home" />
+					</Route>
+					<Route exact path="/login">
+						<Login />
+					</Route>
+					<Route exact path="/portefeuille">
+						<PorteFeuille />
+					</Route>
+					<Route exact path="/historique">
+						<Historique />
+					</Route>
+					<Route exact path="/solde">
+						<Solde />
+					</Route>
+				</IonRouterOutlet>
 			</IonReactRouter>
 		</IonApp>
 	);

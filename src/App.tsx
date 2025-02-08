e23c754784ddc49b8cdf7664ef5f2ca2cebdebb0
El <irina.elina.r@gmail.com>
2025-02-08 22:14:33 +0300

    Back button

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,60 @@
-import { IonApp, setupIonicReact } from '@ionic/react';
+import { IonApp, setupIonicReact, useIonViewDidEnter } from "@ionic/react";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
-/**
- * Ionic Dark Mode
- * -----------------------------------------------------
- * For more info, please see:
- * https://ionicframework.com/docs/theming/dark-mode
- */
-
-/* import '@ionic/react/css/palettes/dark.always.css'; */
-/* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
-
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
-import PushNotificationService from './services/PushNotificationService';
-import { LocalNotifications } from '@capacitor/local-notifications';
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import PushNotificationService from "./services/PushNotificationService";
+import { App as A } from "@capacitor/app";
+import { alertController } from "@ionic/core";
 setupIonicReact();
 
 const App: React.FC = () => {
+  const navigate = useNavigate();
 
-	const navigate = useNavigate()
+  useEffect(() => {
+    const returnListener = () => {
+      document.addEventListener("ionBackButton", async (event) => {
+        if (window.history.state && window.history.state.idx > 0) {
+          navigate(-1); // Go back
+        } else {
+          const alert = await alertController.create({
+            header: "Exit App",
+            message: "Are you sure you want to exit?",
+            buttons: [
+              { text: "Cancel", role: "cancel" },
+              { text: "Exit", handler: () => A.exitApp() },
+            ],
+          });
+          await alert.present();
+        }
+      });
+    };
 
-	useEffect(() => {
-		PushNotificationService.initialize();
-		LocalNotifications.schedule({
-			notifications: [
-				{
-				  title: "title",
-				  body: "body",
-				  id: PushNotificationService.getRandomId(),
-				  schedule: { at: new Date(new Date().getTime() + 1000) }, // 1 sec delay
-				  sound: null,
-				  attachments: null,
-				  actionTypeId: "",
-				  extra: null,
-				  smallIcon: "base_small",
-				},
-			  ],
-		})
-		PushNotificationService.subscribeTo('nouveau_topic');
-		navigate("/login")
-	  }, []);
+	returnListener();
+  }, [navigate]);
 
-	return (
-		<IonApp>
-		</IonApp>
-	);
+  useEffect(() => {
+    PushNotificationService.initialize();
+    PushNotificationService.subscribeTo("nouveau_topic");
+    navigate("/login");
+  }, []);
+
+  return <IonApp></IonApp>;
 };
 
 export default App;
